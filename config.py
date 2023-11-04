efa35a146cbaf8eb6663fed55a94f1de70abfcfc
Ethio Ai <solomonadonay1@gmail.com>
2023-11-04 10:35:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@
 BOT_TOKEN ='6730481569:AAFxQVC3hmrrhYLhWnCS-K_XfrWh4_ci1Oc' # bot token from t.me/botfather
 API_ID = 28153993 #api id from telegram.org
 API_HASH = '976fd7cc4958ad84181a53b41919564b' #api hash from telegram.org
+ALLBOTS = 'https://t.me/ethioaiofficial/585'
